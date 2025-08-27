38ab8db5849f896cde310a884c3ffd2288b09244
Callum Morich <morich.callum@gmail.com>
2025-08-27 21:32:01 +0800

    basic program functional

--- program.py
+++ program.py
@@ -3,9 +3,10 @@
 # Coptyright 2025 Callum Morich
 
 file = open("prompts.txt", "r") # Need to add encoding
-
 prompt_list = file.readlines()
-
 file.close()
 
+list_len = len(prompt_list)
+
+print(list_len)
 print(prompt_list)
