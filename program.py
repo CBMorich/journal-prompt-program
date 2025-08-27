4313dcf649c3ca03854cb8f53fb5818ff1d460c3
CBMorich <morich.callum@gmail.com>
2025-08-27 22:12:10 +0800

    write response works, but with bug

--- program.py
+++ program.py
@@ -12,5 +12,10 @@ list_len = len(prompt_list) - 1
 
 prompt_num = random.randint(0, list_len)
 
-print(prompt_list[prompt_num])
+# print(prompt_list[prompt_num])
+user_response = input(prompt_list[prompt_num])
 
+file = open("responses.txt", "a")
+file.write(user_response) # This works, but writes to the final character
+
+file.close()
