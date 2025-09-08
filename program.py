33922c1ba4d592499b72fd41757ef5c86becaa4e
CBMorich <morich.callum@gmail.com>
2025-09-08 20:16:00 +0800

    feat: add loop to run and exit program

--- program.py
+++ program.py
@@ -8,9 +8,21 @@ from datetime import datetime
 with open("prompts.txt", "r", encoding="utf-8") as file:
     prompt_list = file.readlines()
 
-prompt = random.choice(prompt_list)
-user_response = input(prompt)
+run_program = True
 
-with open("responses.txt", "a", encoding="utf-8") as file:
-    timestamp = datetime.now().strftime("%a %d-%m-%y, %I:%M%p")
-    file.write(timestamp + "\n" + prompt + "\n" + user_response + "\n==========\n")
+while run_program == True:
+    print("<x> to close program")
+    print("<j> to recieve prompt")
+    user_response = input(">>>")
+    if user_response == "x":
+        print("Program Closed")
+        run_program = False
+    elif user_response == "j":
+        prompt = random.choice(prompt_list)
+        user_response = input(prompt)
+        with open("responses.txt", "a", encoding="utf-8") as file:
+            timestamp = datetime.now().strftime("%a %d-%m-%y, %I:%M%p")
+            file.write(timestamp + "\n" + prompt + "\n" + user_response + "\n==========\n")
+    else:
+        print("I didn't understand, please try again")
+
